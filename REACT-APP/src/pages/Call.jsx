7251ff91aba89395369de2cd8192c18b28cac053
RyanGertz <ryan.gertz1@gmail.com>
2024-02-23 18:09:01 +0000

    using darrens endpoint

--- REACT-APP/src/pages/Call.jsx
+++ REACT-APP/src/pages/Call.jsx
@@ -23,12 +23,42 @@ export default function Call(){
     const WS_URL = 'wss://cv372khba8.execute-api.us-west-2.amazonaws.com/production/';
     const {  lastMessage, sendMessage, lastJsonMessage, readyState, sendJsonMessage }  = useWebSocket(WS_URL);
     const [mockData,setMockData] = useState(null)
+    const[darrenData, setDarrenData] = useState(null)
 
 
     if ( lastMessage && lastJsonMessage ){
         console.log(lastMessage);
         console.log(lastJsonMessage);
     }
+    
+    
+    const getDarrenData = async () => {
+    try {
+      const response = await fetch('https://spdcare.calpoly.io/classifiedResults.json');
+      if (!response.ok) {
+        throw new Error(`HTTPS error! status: ${response.status}`);
+      }
+      const calls = await response.json();
+      return calls;
+    } catch (error) {
+      console.error("Could not fetch the calls:", error.message);
+    }
+  };
+     
+    
+    useEffect(() => {
+        const setter = async () =>{
+            const data = await getDarrenData();
+            setDarrenData(data);
+        }
+        setter()
+        }, []);
+        
+    useEffect(() => {
+        if (darrenData) {
+            console.log(darrenData);
+        }
+    }, [darrenData]);
     
     const connectionStatus = {
     [ReadyState.CONNECTING]: 'Connecting',
@@ -90,6 +120,11 @@ export default function Call(){
             <span>Required</span>
             <img src={policeResponse}  onClick={() => navigate(`/PoliceInfo/${"policeResponse"}`)}/>
             <span>{Math.min(policeConfidence * 100,100)}%</span>
+            <ol>
+              <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditatedfaasdfsdfasdfasdf</li>
+              <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+              <li>Lorem ipsum dolor sit amet consectetur adipisicing.</li>
+            </ol>
         </div> : null}
 
         {mockData && mockData["Co-Responder"] ? 
@@ -97,21 +132,31 @@ export default function Call(){
             <span>Co-Responder</span>
             <img src= {coResponser} onClick={() => navigate(`/PoliceInfo/${"coResponser"}`)} />
             <span>{Math.min(coResponserConfidence*100,100)}%</span>
+            <ol>
+              <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate</li>
+              <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+              <li>Lorem ipsum dolor sit amet consectetur adipisicing.</li>
+            </ol>
         </div> : null}
 
         {mockData && mockData["No-issue"] ? 
         <div className='NoIssue-Container' style={{transform: `scale(${Math.min(1 + noIssueConfidence,2)})`}}>
-            <span>No Safety Issue</span>
-            <img src={noIssue} onClick={() => navigate(`/PoliceInfo/${"noIssue"}`)}/>
-            <span>{Math.min(noIssueConfidence*100,100)}%</span>
+          <span>No Safety Issue</span>
+          <img src={noIssue} onClick={() => navigate(`/PoliceInfo/${"noIssue"}`)}/>
+          <span>{Math.min(noIssueConfidence*100,100)}%</span>
         </div>  : null }
 
        {mockData && mockData["Alt-Response"] ? 
        <div className='AltResponse-Container' style={{transform: `scale(${Math.min(1 + altResponseConfidence,2)})`}}>
-            <span>Alternate</span>
-            <span>Response</span>
-            <img src={altResponse} onClick={() => navigate(`/PoliceInfo/${"altResponse"}`)}/>
-            <span>{Math.min(altResponseConfidence*100,100)}%</span>
+          <span>Alternate</span>
+          <span>Response</span>
+          <img src={altResponse} onClick={() => navigate(`/PoliceInfo/${"altResponse"}`)}/>
+          <span>{Math.min(altResponseConfidence*100,100)}%</span>
+          <ol>
+              <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate</li>
+              <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+              <li>Lorem ipsum dolor sit amet consectetur adipisicing.</li>
+          </ol>
         </div> : null }
         
         </div>
