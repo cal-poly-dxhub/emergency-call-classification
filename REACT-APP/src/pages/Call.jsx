de1b84599a74d4c3f97cd886dbe888c7b7dffe45
RyanGertz <ryan.gertz1@gmail.com>
2024-03-10 18:39:51 +0000

    integrated call page with web socket

--- REACT-APP/src/pages/Call.jsx
+++ REACT-APP/src/pages/Call.jsx
@@ -1,7 +1,7 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
 import useWebSocket, { ReadyState } from 'react-use-websocket'
-import throttle from 'lodash.throttle'
+// import throttle from 'lodash.throttle'
 
 
 //css 
@@ -18,66 +18,99 @@ export default function Call(){
     const [noIssueConfidence, setNoIssueConfidence] = useState(0);
     const [coResponserConfidence, setCoResponserConfidence] = useState(0);
     const [altResponseConfidence, setAltResponseConfidence] = useState(0);
-    const[callInstructions,setCallInstructions] = useState("");
-    const { id } = useParams();
+    const [callInstructions,setCallInstructions] = useState("");
+    const { id } = useParams(); //call id from landing page
     const navigate = useNavigate();
-    const WS_URL = 'wss://cv372khba8.execute-api.us-west-2.amazonaws.com/production/';
-    const {  lastMessage, sendMessage, lastJsonMessage, readyState, sendJsonMessage }  = useWebSocket(WS_URL);
-    const [mockData,setMockData] = useState(null)
     const[darrenData, setDarrenData] = useState(null)
+    const loadingTexts = ['Predicting','Predicting.', 'Predicting..', 'Predicting...'];
+    const [currentText, setCurrentText] = useState(0);
+    const WS_URL = 'wss://cv372khba8.execute-api.us-west-2.amazonaws.com/production/';
+    const didUnmount = useRef(false)
+    const { lastJsonMessage, readyState, sendJsonMessage }  = useWebSocket(WS_URL, {
+      queryParams: {id},
+      shouldReconnect: (closeEvent) => {
+        return didUnmount.current === false;
+      },
+      reconnectAttempts: 10,
+      reconnectInterval: 3000 //ms
+    });
 
 
-    if ( lastMessage && lastJsonMessage ){
-        console.log(lastMessage);
+    
+    
+    
+    useEffect(()=>{
+      if (lastJsonMessage){
         console.log(lastJsonMessage);
+        var coresponderValue = parseFloat(lastJsonMessage.M.CORESPONDER.N);
+        var noIssueValue = parseFloat(lastJsonMessage.M.NOISSUE.N);
+        var policeValue = parseFloat(lastJsonMessage.M.POLICE.N);
+        setDarrenData(lastJsonMessage)
+        
+        setPoliceConfidence(Math.round((parseFloat(lastJsonMessage?.M?.POLICE?.N) ?? 0) *100));
+        setNoIssueConfidence(Math.round((parseFloat(lastJsonMessage?.M?.NOISSUE?.N) ?? 0) *100))
+        setCoResponserConfidence(Math.round((parseFloat(lastJsonMessage?.M?.CORESPONDER?.N) ?? 0) *100))
+        setAltResponseConfidence(Math.round((parseFloat(lastJsonMessage?.M?.ALTERNATE?.N) ?? 0) *100))
+
+        console.log(coresponderValue, noIssueValue, policeValue);
+        // setDarrenData(lastJsonMessage)
     }
+    },[lastJsonMessage])
+    
+  //   const getDarrenData = async () => {
+  //   try {
+  //     const url = new URL('https://spdcare.calpoly.io/classifiedResults.json'); //so the browser does not cache
+  //     url.searchParams.append('nocache', new Date().getTime());
     
     
-    const getDarrenData = async () => {
-    try {
-      const url = new URL('https://spdcare.calpoly.io/classifiedResults.json'); //so the browser does not cache
-      url.searchParams.append('nocache', new Date().getTime());
-    
-    
-      const response = await fetch(url.toString());
-      if (!response.ok) {
-        throw new Error(`HTTPS error! status: ${response.status}`);
-      }
-      const calls = await response.json();
-      return calls;
-    } catch (error) {
-      console.error("Could not fetch the calls:", error.message);
-    }
-  };
+  //     const response = await fetch(url.toString());
+  //     if (!response.ok) {
+  //       throw new Error(`HTTPS error! status: ${response.status}`);
+  //     }
+  //     const calls = await response.json();
+  //     return calls;
+  //   } catch (error) {
+  //     console.error("Could not fetch the calls:", error.message);
+  //   }
+  // };
      
+    // useEffect(()=>{
+    //   if(callInstructions != ""){
+    //     navigate("/PoliceInfo");
+    //   }
+    // },[callInstructions,navigate])
     
-    useEffect(() => {
-        const setter = async () =>{
-            const data = await getDarrenData();
-            setDarrenData(data);
-        }
-        setter()
-        }, []);
+    // useEffect(() => {
+    //     const setter = async () =>{
+    //         const data = await getDarrenData();
+    //         setDarrenData(data);
+    //     }
+    //     setter()
+    //     }, []);
         
     useEffect(() => {
         if (darrenData) {
-            console.log(darrenData);
-            console.log(darrenData["POLICE"])
-            console.log(darrenData["CORESPONDER"])
-            console.log(darrenData["NOISSUE"])
+            // console.log(darrenData);
+            // console.log(darrenData["POLICE"])
+            // console.log(darrenData["CORESPONDER"])
+            // console.log(darrenData["NOISSUE"])
             // console.log(darrenData["Instructions"])
-            console.log(darrenData["ALTERNATE"])
+            // console.log(darrenData["ALTERNATE"])
             if(darrenData["ALTERNATE"]){
               setAltResponseConfidence(Math.round(darrenData["ALTERNATE"] * 100));
+              //setAltResponseConfidence((darrenData["ALTERNATE"]*100).toFixed(2));
             }
             if(darrenData["CORESPONDER"]){
               setCoResponserConfidence(Math.round(darrenData["CORESPONDER"] * 100));
+              //setCoResponderConfidence((darrenData["CORESPONDER"]*100).toFixed(2));
             }
             if(darrenData["POLICE"]){
               setPoliceConfidence(Math.round(darrenData["POLICE"] * 100));
+              // setPoliceConfidence((darrenData["POLICE"]*100).toFixed(2))
             }
             if(darrenData["NOISSUE"]){
               setNoIssueConfidence(Math.round(darrenData["NOISSUE"] * 100));
+              //setNoIssueConfidence(darrenData["NOISSUE"]*100).toFixed(2));
             }
             setCallInstructions(darrenData["Instructions"]);
         }
@@ -91,95 +124,61 @@ export default function Call(){
     [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
   }[readyState];
   
-  // const getMock = async () => {
-  //   try {
-  //     const response = await fetch('https://my-json-server.typicode.com/ryangertz/testdb3/confidence');
-  //     if (!response.ok) {
-  //       throw new Error(`HTTP error! status: ${response.status}`);
-  //     }
-  //     const calls = await response.json();
-  //     return calls;
-  //   } catch (error) {
-  //     console.error("Could not fetch the calls:", error.message);
-  //   }
-  // };
   
-  //   useEffect(() => {
-  //       const setter = async () =>{
-  //           const data = await getMock();
-  //           setMockData(data);
-  //       }
-  //       setter()
-  //       }, []);
-        
-  //   useEffect(() => {
-  //       if (mockData) {
-  //           setAltResponseConfidence(mockData["Alt-Response"]);
-  //           setCoResponserConfidence(mockData["Co-Responder"]);
-  //           setPoliceConfidence(mockData["Police-Response"]);
-  //           setNoIssueConfidence(mockData["No-issue"]);
-  //       }
-  //   }, [mockData]);
-  
-    function Instructions({ text }) {
-  // Split the text into lines
-  const lines = text.split('\n');
-  // console.log(lines);
-  return (
-    <ol>
-      {lines.map((line, index) => (
-        // Use index as a key; it's acceptable here since the list is static
-        <li key={index}>{line}</li>
-      ))}
-    </ol>
-  );
-}
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentText((prevText) => (prevText + 1) % loadingTexts.length);
+    }, 500); // Update every 500 milliseconds
+
+    return () => clearInterval(interval);
+  }, []);
 
     return(
     <div className='Call'>
 
       <div className='Title-Container'>
         <h1>Caller ID: {id}</h1>
+        <h2 className='Pending'>{loadingTexts[currentText]}</h2>
       </div>
         
-      {/* <button onClick={()=>{sendJsonMessage({ action: "sendMessage", data: "hello world" })}}>click me</button>
+       {/*<button onClick={()=>{sendJsonMessage({ action: "sendMessage", data: "hello world" })}}>click me</button> */}
         
-        <span>The WebSocket is currently {connectionStatus}</span> */}
+        <span>The WebSocket is currently {connectionStatus}</span> 
        
         
       <div className='Content'>
-        {darrenData && darrenData["POLICE"] ? 
+        {darrenData && darrenData.M.POLICE ? 
        <div className='Police-Container' style={{transform: `scale(${Math.min((policeConfidence/100)+1,2)})`}}>
             <span>Police Response</span>
             <span>Required</span>
             <img src={policeResponse}  onClick={() => navigate(`/PoliceInfo/${"policeResponse"}`)}/>
             <span>{Math.min(policeConfidence,100)}%</span>
-            <Instructions text={callInstructions} />
+           
         </div> : null}
 
-        {darrenData && darrenData["CORESPONDER"] ? 
+        {darrenData && darrenData.M.CORESPONDER ? 
         <div className='CoResponder-Container' style={{transform: `scale(${Math.min((coResponserConfidence/100)+1,2)})`}}>
             <span>Co-Responder</span>
             <img src= {coResponser} onClick={() => navigate(`/PoliceInfo/${"coResponser"}`)} />
             <span>{Math.min(coResponserConfidence,100)}%</span>
-            <Instructions text={callInstructions} />
+          
         </div> : null}
 
-        {darrenData && darrenData["NOISSUE"] ? 
+        {darrenData && darrenData.M.NOISSUE ? 
         <div className='NoIssue-Container' style={{transform: `scale(${Math.min((noIssueConfidence/100)+1,2)})`}}>
           <span>No Safety Issue</span>
           <img src={noIssue} onClick={() => navigate(`/PoliceInfo/${"noIssue"}`)}/>
           <span>{Math.min(noIssueConfidence,100)}%</span>
-          <Instructions text={callInstructions} />
+        
         </div>  : null }
 
-       {darrenData && darrenData["ALTERNATE"] ? 
+       {darrenData && darrenData.M.ALTERNATE ? 
        <div className='AltResponse-Container' style={{transform: `scale(${Math.min((altResponseConfidence/100)+1,2)})`}}>
           <span>Alternate</span>
           <span>Response</span>
           <img src={altResponse} onClick={() => navigate(`/PoliceInfo/${"altResponse"}`)}/>
           <span>{Math.min(altResponseConfidence,100)}%</span>
-          <Instructions text={callInstructions} />
+       
         </div> : null }
         
       </div>
