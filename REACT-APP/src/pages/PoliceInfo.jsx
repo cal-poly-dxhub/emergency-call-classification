20f35aecc58f727b08c2699be1ce690917352688
RyanGertz <ryan.gertz1@gmail.com>
2024-03-10 18:40:51 +0000

    dynamic text for call info page

--- REACT-APP/src/pages/PoliceInfo.jsx
+++ REACT-APP/src/pages/PoliceInfo.jsx
@@ -16,22 +16,27 @@ export default function PoliceInfo(){
     const { issue } = useParams();
     let pic;
     let clr;
+    let title;
     
     if (issue === "policeResponse"){
         pic = policeResponse;
         clr = "#E84F70";
+        title = "Police Response Required"
     }
     else if (issue === "coResponser"){
         pic = coResponser;
         clr = "#F2C46D";
+        title = "Co-Responder Response Required"
     }
     else if (issue === "noIssue"){
         pic = noIssue;
         clr = "#41D9A1";
+        title = "No Safety Issue"
     }
     else if (issue === "altResponse"){
         pic = altResponse;
         clr = "#268EA6";
+        title = "Alternate Response Required"
     }
 
 
@@ -44,7 +49,7 @@ export default function PoliceInfo(){
                 
                     <div className='Title-Img-Container'>
                     <img src={pic} /> 
-                        <h1 style={{color: clr}}>Police Response Required</h1>
+                        <h1 style={{color: clr}}>{title}</h1>
                     </div>
 
                     <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, commodi est sapiente enim aliquam tempore fugit accusantium possimus nisi, quo quos rem blanditiis natus tenetur ipsum similique labore ullam modi!</li>
